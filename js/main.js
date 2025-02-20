9f42f0aaba461cc16fa80b83d039e9bcadb26e77
AlzXploit <fahreja624@gmail.com>
2025-02-20 09:05:37 -0800

    menambahkah fitur keahlian dan flip

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 function demo() {
     alert("Mohon Maaf untuk saat ini fitur masih dikembangkan oleh developer")
 }
+function flipCard(card) {
+    card.classList.toggle("flipped");
+}
+
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault(); // Blokir klik kanan
+});
+
+document.addEventListener("copy", function (e) {
+    e.preventDefault(); // Blokir Ctrl+C atau copy di menu
+});
+
+document.addEventListener("dragstart", function (e) {
+    e.preventDefault(); // Blokir drag gambar
+});
